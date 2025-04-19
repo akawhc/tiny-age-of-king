c9f5071cfdb2d6c9709cf7f88cc421dad6bf7804
ponywu <ponywu@tencent.com>
2025-04-19 15:52:52 +0800

    feat: 资源类碰撞检测统一使用基类的方法

--- scripts/resource/wood.gd
+++ scripts/resource/wood.gd
@@ -25,7 +25,7 @@ func _ready() -> void:
 	super._ready()  # 调用父类的_ready方法
 	add_to_group("resources")  # 将木材资源添加到resources组
 	_play_drop_animation()
-	body_entered.connect(_on_body_entered)
+	# body_entered.connect(_on_body_entered)
 
 func _play_drop_animation() -> void:
 	var tween = create_tween()
@@ -41,16 +41,6 @@ func _play_drop_animation() -> void:
 		position,
 		WOOD_CONFIG.drop_animation.down_time
 	)
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("workers"):
-		if body.has_method("collect_wood"):
-			if body.name == "worker":  # 如果是工人，则等待工人收集
-				# 先通知工人收集木材
-				body.collect_wood(self)  # 传递木材实例给工人
-	else:
-		# 其他玩家就直接让木材消失
-		_play_collect_animation()
 
 func _play_collect_animation() -> void:
 	var tween = create_tween()
